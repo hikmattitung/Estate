dddc52bdddac9ecdb204f3f6ac0867d1e4a403cd
Hikmat Tamang <hikmattitung1111@gmail.com>
2026-04-13 21:06:03 +0545

    commit about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,21 +7,21 @@ const About = () => {
         About Hikmat Estate
       </h1>
 
-      <p className="mb-4 text-slate-500">
+      <p className="mb-4 text-slate-600">
         Hikmat Tamang Estate is a modern and trusted real estate platform based in World,is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of skilled professionals is committed to delivering exceptional service
       </p>
 
-      <p className="mb-4 text-slate-500">
+      <p className="mb-4 text-slate-600">
         Our mission is to help clients achieve their real estate goals by
         providing expert guidance, transparent service, and strong knowledge
         of the local property market. We focus on making property buying and
-        selling simple and stress-free thank you!!!dsjhbadadaj
+        selling simple and stress-free.
       </p>
 
-      <p className="mb-4 text-slate-500">
+      <p className="mb-4 text-slate-600">
         Our experienced team is committed to delivering high-quality service.
         We believe real estate should be a smooth and rewarding journey for
-        every client we work with. This much for today we can fix later okay?? later na bye byedsadasdasdsjhdgsdjgsfgsjf
+        every client we work with. This much for today we can fix later okay?? 
       </p>
     </div>
   );
